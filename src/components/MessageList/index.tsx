6fa3616007ddb253f5007e94933ff55ab3e1cfec
Jin B. Oliveira <jinoliveira74@gmail.com>
2021-10-20 23:28:41 -0300

    chore: listando mensagens

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,47 +1,47 @@
 import styles from './styles.module.scss'
 
 import logoImg from "../../assets/logo.svg"
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+
+type Message = {
+   id: string;
+   text: string;
+   user: {
+      avatar_url: string;
+      name: string;
+   }
+}
 
 export function MessageList() {
+   const [messages, setMessages] = useState<Message[]>([])
+
+   useEffect(() => {
+      api.get<Message[]>("messages/last3").then(response => {
+         setMessages(response.data)
+      })
+   }, [])
    return (
       <div className={styles.messageListWrapper}>
          <img className={styles.logo} src={logoImg} alt="DoWhile 2021" />
 
          <ul className={styles.messageList}>
-            <li className={styles.message}>
-               <p className={styles.messageContent}>
-                  Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+            {messages.map(message => (
+               <li className={styles.message} key={message.id}>
+                  <p className={styles.messageContent}>
+                     {message.text}
+                  </p>
                   <div className={styles.messageUser}>
                      <div className={styles.userImage}>
-                        <img src="https://github.com/jin-cloud-max.png" alt="Jin" />
+                        <img
+                           src={message.user.avatar_url}
+                           alt={message.user.name}
+                        />
                      </div>
-                     <span>Jin Oliveira</span>
+                     <span>{message.user.name}</span>
                   </div>
-               </p>
-            </li>
-
-            <li className={styles.message}>
-               <p className={styles.messageContent}>
-                  Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                  <div className={styles.messageUser}>
-                     <div className={styles.userImage}>
-                        <img src="https://github.com/jin-cloud-max.png" alt="Jin" />
-                     </div>
-                     <span>Jin Oliveira</span>
-                  </div>
-               </p>
-            </li>
-            <li className={styles.message}>
-               <p className={styles.messageContent}>
-                  Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                  <div className={styles.messageUser}>
-                     <div className={styles.userImage}>
-                        <img src="https://github.com/jin-cloud-max.png" alt="Jin" />
-                     </div>
-                     <span>Jin Oliveira</span>
-                  </div>
-               </p>
-            </li>
+               </li>
+            ))}
          </ul>
       </div>
    )
