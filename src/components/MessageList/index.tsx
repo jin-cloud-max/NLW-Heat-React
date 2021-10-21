3912db77c3a3fd222863c83acbea3a2362269c8a
Jin B. Oliveira <jinoliveira74@gmail.com>
2021-10-20 23:07:31 -0300

    chore: layout da pagina criada

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,48 @@
+import styles from './styles.module.scss'
+
+import logoImg from "../../assets/logo.svg"
+
+export function MessageList() {
+   return (
+      <div className={styles.messageListWrapper}>
+         <img className={styles.logo} src={logoImg} alt="DoWhile 2021" />
+
+         <ul className={styles.messageList}>
+            <li className={styles.message}>
+               <p className={styles.messageContent}>
+                  Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                  <div className={styles.messageUser}>
+                     <div className={styles.userImage}>
+                        <img src="https://github.com/jin-cloud-max.png" alt="Jin" />
+                     </div>
+                     <span>Jin Oliveira</span>
+                  </div>
+               </p>
+            </li>
+
+            <li className={styles.message}>
+               <p className={styles.messageContent}>
+                  Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                  <div className={styles.messageUser}>
+                     <div className={styles.userImage}>
+                        <img src="https://github.com/jin-cloud-max.png" alt="Jin" />
+                     </div>
+                     <span>Jin Oliveira</span>
+                  </div>
+               </p>
+            </li>
+            <li className={styles.message}>
+               <p className={styles.messageContent}>
+                  Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                  <div className={styles.messageUser}>
+                     <div className={styles.userImage}>
+                        <img src="https://github.com/jin-cloud-max.png" alt="Jin" />
+                     </div>
+                     <span>Jin Oliveira</span>
+                  </div>
+               </p>
+            </li>
+         </ul>
+      </div>
+   )
+}
